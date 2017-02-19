31f85ca7bcca393c74d4526e60513518e8799e38
Anonrate <Anonrate@outlook.com>
2017-02-19 01:25:07 -0500

    Fixed header format.

--- inc/lgr.h
+++ inc/lgr.h
@@ -5,24 +5,24 @@
  * @date	02/15/2017 17:10:55
  * @author	Anonrate
  * @copyright	
- * 	\parblock
- * 		GNU General Public License
+ *  \parblock
+ * 	  GNU General Public License
  *
- * 		Copyright (C) 2017 Anonrate
+ * 	  Copyright (C) 2017 Anonrate
  *
- * 		This program is free software: you can redistribute it and/or modify 
- * 		it under the terms of the GNU General Public License as published by 
- * 		the Free Software Foundation, either version 3 of the License, or 
- * 		(at your option) any later version.
+ * 	  This program is free software: you can redistribute it and/or modify 
+ * 	  it under the terms of the GNU General Public License as published by 
+ * 	  the Free Software Foundation, either version 3 of the License, or 
+ * 	  (at your option) any later version.
  *
- * 		This program is distributed in the hope that it will be useful, 
- * 		but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * 		GNU General Public License for more details.
+ * 	  This program is distributed in the hope that it will be useful, 
+ * 	  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * 	  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * 	  GNU General Public License for more details.
  *
- * 		You should have received a copy of the GNU General Public License
- * 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 	\endparblock
+ * 	  You should have received a copy of the GNU General Public License
+ * 	  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  \endparblock
  */
 
 #ifndef LGR_H
