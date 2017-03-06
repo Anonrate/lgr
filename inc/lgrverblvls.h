2ee5382e5011aa47c3f0ffe811d4267f65586628
Anonrate <Anonrate@outlook.com>
2017-03-05 19:21:20 -0500

    Got rid of comments.  Was anoying.

--- inc/lgrverblvls.h
+++ inc/lgrverblvls.h
@@ -28,71 +28,36 @@
 #ifndef LGRVERBLVLS_H
 #define LGRVERBLVLS_H /*< Include guard for 'lgrverblvls.h' */
 
-/**
- *  #NVALID_VERB_LVL string
- */
 #define NVALID_VERB_LVL_STR "NVALID_VERB_LVL"
 
-/*
- *  Each same line document in the following definitions, do not have a 'valid'
- *    'tabstop' after the word 'string' because even if to do so, that would
- *    make the comment closer at column 80, and that is greater than 79
- *    (Obviously), and my coding style is to keep everything under or at max 79
- *    characters long for each line as said so in the GNU formatting docs.  Yes
- *    I follow a lot of the conventions there, but not all.
- */
-#define FATAL_STR           "FATAL"           /**< #FATAL          string */
-#define ERROR_STR           "ERROR"           /**< #ERROR          string */
-#define WARNING_STR         "WARNING"         /**< #WARNING        string */
-#define NOTICE_STR          "NOTICE"          /**< #NOTICE         string */
-#define INFO_STR            "INFO"            /**< #INFO           string */
-#define DEBUG_STR           "DEBUG"           /**< #DEBUG          string */
-#define TRACE_STR           "TRACE"           /**< #TRACE          string */
-#define INTERN_WARNING_STR  "INTERN_WARNING"  /**< #INTERN_WARNING string */
-#define INTERN_INFO_STR     "INTERN_INFO"     /**< #INTERN_INFO    string */
-#define INTERN_DEBUG_STR    "INTERN_DEBUG"    /**< #INTERN_DEBUG   string */
-#define INTERN_TRACE_STR    "INTERN_TRACE"    /**< #INTERN_TRACE   string */
+#define FATAL_STR           "FATAL"
+#define ERROR_STR           "ERROR"
+#define WARNING_STR         "WARNING"
+#define NOTICE_STR          "NOTICE"
+#define INFO_STR            "INFO"
+#define DEBUG_STR           "DEBUG"
+#define TRACE_STR           "TRACE"
+#define INTERN_WARNING_STR  "INTERN_WARNING"
+#define INTERN_INFO_STR     "INTERN_INFO"
+#define INTERN_DEBUG_STR    "INTERN_DEBUG"
+#define INTERN_TRACE_STR    "INTERN_TRACE"
 
-/**
- *  @brief   verbosity levels
- *
- *  Enumerator containing constants specifying each verbosity level.
- */
 enum
 verblvls
 {
-    /**
-     *  Used when a verbose level is given, and it is not a valid \link
-     *    verblvls verbose level\endlink.
-     */
-    NVALID_VERB_LVL = 0x0,  /**< @brief invalid verbose level */
+    NVALID_VERB_LVL = 0x0,
 
-    FATAL   = 0x1,  /**< Fatal messages.        */
-    ERROR   = 0x2,  /**< Error messages.        */
-    WARNING = 0x3,  /**< Warning messages.      */
-    NOTICE  = 0x4,  /**< Notice messages.       */
-    INFO    = 0x5,  /**< Informative messages.  */
-    DEBUG   = 0x6,  /**< Display everything!    */
-    TRACE   = 0x7,  /**< Trace messages.        */
+    FATAL   = 0x1,
+    ERROR   = 0x2,
+    WARNING = 0x3,
+    NOTICE  = 0x4,
+    INFO    = 0x5,
+    DEBUG   = 0x6,
+    TRACE   = 0x7,
 
-    /**
-     *  Used to enable the #WARNING messages of the logger itself.
-     */
-    INTERN_WARNING  = 0x8,  /**< @brief internal warning      */
-
-    /**
-     *  Used to enable the #INFO messages of the logger itself.
-     */
-    INTERN_INFO     = 0x9,  /**< @brief internal information  */
-
-    /**
-     *  Used to enable the #DEBUG messages of the logger itself.
-     */
-    INTERN_DEBUG    = 0xa,  /**< @brief internal debug        */
-
-    /**
-     *  Used to enable the #TRACE messages of the logger itself.
-     */
-    INTERN_TRACE    = 0xb   /**< @brief internal trace        */
+    INTERN_WARNING  = 0x8,
+    INTERN_INFO     = 0x9,
+    INTERN_DEBUG    = 0xa,
+    INTERN_TRACE    = 0xb
 };
 #endif  /* LGRVERBLVLS_H */
