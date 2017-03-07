7bc3cd04df9395f1ddc470da37f695bfe8a1b5ff
Anonrate <Anonrate@outlook.com>
2017-03-06 22:36:35 -0500

    Moved macros

--- inc/loglfxf.h
+++ inc/loglfxf.h
@@ -28,6 +28,16 @@
 #ifndef LOGLFXF_H
 #define LOGLFXF_H
 
+#define loglff(verblvl, fmt, ...)                               \
+    (                                                           \
+        lgrf((verblvl), 0, __FILE__, 0, 0, (fmt), __VA_ARGS__)  \
+    )
+
+#define loglfstr(verblvl, str)                    \
+    (                                             \
+        lgrf((verblvl), 0, __FILE__, 0, 0, (str)) \
+    )
+
 #define loglffnf(verblvl, fmt, ...)                                     \
     (                                                                   \
         lgrf((verblvl), 0, __FILE__, __func__, 0u, (fmt), __VA_ARGS__)  \
