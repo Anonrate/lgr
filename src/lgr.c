acb28531367874079602570e77d4bd45dfabd5e4
Anonrate <Anonrate@Outlook.com>
2017-04-19 15:03:20 -0400

    Took out a whole bunch of shit.

--- src/lgr.c
+++ src/lgr.c
@@ -1,7 +1,7 @@
 /**
  *  @file     lgr.c
  *  @brief    lgr.c
- *  @version  v.4
+ *  @version  v.5
  *  @date     02/15/2017 18:08:02
  *  @author   Anonrate
  *  @copyright
@@ -34,8 +34,9 @@
 #include  <assert.h>
 
 #define LGR_DEV
+#undef  LGR_DEV
 
-#include  "../inc/lgrverblvls.h"
+#include  "../inc/lgr.h"
 
 static const char*
 getvlvln(enum verblvls verblvl)
@@ -82,7 +83,7 @@ static char           *fnout    = "\0";
 
 static FILE           *fout     = 0;
 
-static void
+void
 lgrf(enum   verblvls        verblvl,
      const            char  *timestr,
      const            char  *filestr,
@@ -91,12 +92,7 @@ lgrf(enum   verblvls        verblvl,
      const            char  *strfmt, ...)
 {
     if (!(verblvl > 0
-                && verblvl <=
-#ifdef  LGR_DEV
-                              DEV_INTERN_TRACE
-#else /* !defined LGR_DEV */
-                              TRACE
-#endif  /* LGR_DEV        */
+                && verblvl <= TRACE
          )  ? verblvl
             : NVALID_VERB_LVL) { return; }
 
@@ -114,7 +110,6 @@ lgrf(enum   verblvls        verblvl,
              : stdout));
 
     int doltf = ltf && fout && tmpvlvl <= fprio;
-
     if (timestr)
     {
         fprintf(fpstrm,   "[%s]  %-18s:  ", timestr,  getvlvln(verblvl));
@@ -160,19 +155,23 @@ lgrf(enum   verblvls        verblvl,
     va_end(ap);
 }
 
-#include  "../inc/lgr.h"
+//#include  "../inc/lgr.h"
 
 #ifndef NAME_MAX
 #define NAME_MAX  0xfe
 #endif  /* NAME_MAX */
 
-#include  "../inc/lgrmsgs.h"
+//#include  "../inc/lgrmsgs.h"
 
 #ifdef  LGR_DEV
 int
 main(int argc, char **argv)
 {
+    setverblvl(TRACE);
+    setfileprio(TRACE);
+    setlogtofile(1);
     setfilename("lgr");
+    logltffnlf(INFO, "%s\n", "Doing this shit.");
     return EXIT_SUCCESS;
 }
 #endif  /* LGR_DEV    */
@@ -181,490 +180,161 @@ main(int argc, char **argv)
 const char*
 getverblvlname(enum verblvls verblvl)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "enum verblvls verblvl = %hhu\n", verblvl);
-#endif  /* LGR_DEV                */
-
-    CALLFN_MSGLS(INTERN_TRACE, "getvlvln()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_TRACE,
-               "enum verblvls verblvl = verblvl(%hhu)\n",
-               verblvl);
-#endif  /* LGR_DEV                */
-    const     char *tmpvlvln  = getvlvln(verblvl);
-
-    unsigned  char  tmpvlvl   = INTERN_INFO;
-              char  *tmpstr   = ((strcmp(tmpvlvln, NVALID_VERB_LVL_STR))
-                              ? " "
-                              : (tmpvlvl = WARNING, " not "));
-    logltffnlf(tmpvlvl, "%hhu is%sa valid verbose level!\n", verblvl, tmpstr);
-
-    R_MSGLS(INTERN_DEBUG, tmpvlvln);
-    return tmpvlvln;
+    return getvlvln(verblvl);
 }
 
 int
 isverblvl(unsigned char lvl)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "unsigned char lvl = %hhu\n", lvl);
-#endif  /* LGR_DEV                */
-
-    CALLFN_MSGLS(INTERN_TRACE, "getverblvlname()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_TRACE, "enum verblvls verblvl = lvl(%hhu)\n", lvl);
-#endif  /* LGR_DEV                */
-    if (strcmp(getverblvlname(lvl), NVALID_VERB_LVL_STR))
-    {
-        R_MSGLHHU(INTERN_DEBUG, NVALID_VERB_LVL);
-        return NVALID_VERB_LVL;
-    }
-
-    R_MSGLHHU(INTERN_DEBUG, lvl);
-    return (int)lvl;
+    return (strcmp(getverblvlname(lvl), NVALID_VERB_LVL_STR)
+            ? (int)lvl
+            : NVALID_VERB_LVL);
 }
 
 static void
-mallstr(const char *stra, char **pstrb, char *strbn)
+mallstr(const char *stra, char **pstrb)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "const char *stra   = %s\n", stra);
-    logltffnlf(DEV_INTERN_DEBUG, "      char **pstrb = %s\n", *pstrb);
-    logltffnlf(DEV_INTERN_DEBUG, "      char *strbn  = %s\n", strbn);
-#endif  /* LGR_DEV                */
-
     size_t tmpstrbsz  = strlen(*pstrb);
     size_t tmpstrasz  = strlen(stra);
-    if (tmpstrbsz != tmpstrasz) {
-        if (!(*pstrb = malloc(tmpstrasz + 1ul))) {
-            fatalf("Failed to allocate %lu + 1 for %s!\n",
-                   tmpstrasz,
-                   "*pstrb");
-        }
-    }
+    if (tmpstrbsz != tmpstrasz) { *pstrb = malloc(tmpstrasz + 1lu); }
 }
 
 static char*
 setvlvln(enum verblvls verblvl)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "enum verblvls verblvl = %hhu\n", verblvl);
-#endif  /* LGR_DEV                */
-
-    CALLFN_MSGLS(INTERN_TRACE, "getverblvlname()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_TRACE,
-               "enum verblvls verblvl = verblvl(%hhu)\n",
-               verblvl);
-#endif  /* LGR_DEV                */
     const char *tmpvlvln = getverblvlname(verblvl);
-    if (!strcmp(vlvln, tmpvlvln))
-    {
-        R_MSGLS(INTERN_DEBUG, vlvln);
-        return vlvln;
-    }
-
+    if (!strcmp(vlvln, tmpvlvln)) { return vlvln; }
     if (strcmp(tmpvlvln, NVALID_VERB_LVL_STR))
     {
-        CALLFN_MSGLS(INTERN_TRACE, "mallstr()");
-#ifdef  LGR_DEV
-        MALLSTR_DEVMSGSS(tmpvlvln, vlvln);
-#endif  /* LGR_DEV                */
-        mallstr(tmpvlvln, &vlvln, "vlvln");
-
-        SET_MSGLSS(INTERN_INFO, vlvln, tmpvlvln);
+        mallstr(tmpvlvln, &vlvln);
         strcpy(vlvln, tmpvlvln);
-
-        R_MSGLS(INTERN_DEBUG, vlvln);
-        return vlvln;
     }
 
-    R_MSGLS(INTERN_DEBUG, vlvln);
     return vlvln;
 }
 
 static unsigned char
 setvlvl(unsigned char verblvl)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "unsigned char verblvl = %hhu\n", verblvl);
-#endif  /* LGR_DEV                */
-
-    SET_MSGLHHUHHU(INTERN_INFO, vlvl, verblvl);
-    vlvl = verblvl;
-
-    R_MSGLHHU(INTERN_DEBUG, verblvl);
-    return (verblvl);
+    return (verblvl = vlvl);
 }
 
 unsigned char
 setverblvl(enum verblvls verblvl)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "enum verblvls verblvl = %hhu\n", verblvl);
-#endif  /* LGR_DEV                */
+    if (vlvl == verblvl) { return vlvl; }
 
-    if (vlvl == verblvl)
-    {
-        R_MSGLHHU(INTERN_DEBUG, vlvl);
-        return vlvl;
-    }
-
-    CALLFN_MSGLS(INTERN_TRACE, "isverblvl()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_TRACE,
-               "unsigned char verblvl = verblvl(%hhu)\n",
-               verblvl);
-#endif  /* LGR_DEV                */
     if (isverblvl(verblvl))
     {
-        CALLFN_MSGLS(INTERN_TRACE, "setvlvl()");
-#ifdef  LGR_DEV
-        logltffnlf(DEV_INTERN_TRACE,
-                   "unsigned char verblvl = verblvl(%hhu)\n",
-                   verblvl);
-#endif  /* LGR_DEV                */
         setvlvl(verblvl);
-
-        CALLFN_MSGLS(INTERN_TRACE, "setvlvln()");
-#ifdef  LGR_DEV
-        logltffnlf(DEV_INTERN_TRACE,
-                   "unsigned char verblvl = verblvl(%hhu)\n",
-                   verblvl);
-#endif  /* LGR_DEV                */
         setvlvln(verblvl);
-
-        CALLFN_MSGLS(INTERN_TRACE, "getverblvlname()");
-#ifdef  LGR_DEV
-        logltffnlf(DEV_INTERN_TRACE,
-                   "enum verblvls verblvl = vlvl(%hhu)\n",
-                   vlvl);
-#endif  /* LGR_DEV                */
-        int ti = strcmp(vlvln, getverblvlname(vlvl));
-        if (ti) { fatalf("Validation failed..  strcmp returned %d.", ti); }
-
-        R_MSGLD(INTERN_DEBUG, vlvl);
-        return vlvl;
     }
-    R_MSGLHHU(INTERN_DEBUG, vlvl);
+
     return vlvl;
 }
 
 enum verblvls
 getverblvl(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-    GET_MSGLS(INTERN_INFO, "vlvl");
-
-    R_MSGLS(INTERN_DEBUG, vlvl);
     return vlvl;
 }
 
 enum verblvls
 getfileprio(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-    GET_MSGLS(INTERN_INFO, "fprio");
-
-    R_MSGLS(INTERN_DEBUG, fprio);
     return fprio;
 }
 
 int
 geterrwarn(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-    GET_MSGLS(INTERN_INFO, "errwarn");
-
-    R_MSGLD(INTERN_DEBUG, errwarn);
     return errwarn;
 }
 
 enum verblvls
 setfileprio(enum verblvls fileprio)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "enum verblvls fileprio = %hhu\n", fileprio);
-#endif  /* LGR_DEV                */
+    if (!isverblvl(fileprio)) { return (unsigned char)0; }
 
-    if (!ltf) { logltffnlf(WARNING, "%s\n", "Logging to file is not set..."); }
-
-    CALLFN_MSGLS(INTERN_TRACE, "isverblvl()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_TRACE,
-               "unsigned char lvl = fileprio(%s)\n",
-               fileprio);
-#endif  /* LGR_DEV                */
-    if (!isverblvl(fileprio))
-    {
-        logltffnlf(ERROR,
-                  "Failed to update file priority.  %hhu is not a valid "
-                  "verbose level.  Leaving file priority as is.(%hhu)\n",
-                  fileprio,
-                  fprio);
-
-        R_MSGLHHU(INTERN_DEBUG, (unsigned char)0);
-        return (unsigned char)0;
-
-    }
-
-    SET_MSGLHHUHHU(INTERN_INFO, fprio, fileprio);
-    fprio = fileprio;
-
-    R_MSGLHHU(INTERN_DEBUG, fprio);
-    return fprio;
+    return (fprio = fileprio);
 }
 
 int
 seterrwarn(int treatwarnerr)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "int treatwarnerr = %d\n", treatwarnerr);
-#endif  /* LGR_DEV                */
-
-    SET_MSGLDD(INTERN_INFO, errwarn, treatwarnerr);
-    errwarn = treatwarnerr;
-
-    R_MSGLD(INTERN_DEBUG, errwarn);
-    return errwarn;
+    return (errwarn = treatwarnerr);
 }
 
 static char*
 setfout(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
+    time_t    t       = time(0);
+    struct tm *ti     = localtime(&t);
+    char      *tmpfno = malloc(NAME_MAX);
 
-    if (!ltf) { logltffnlf(WARNING, "%s\n", "Logging to file is not set..."); }
-    if (!fname)
-    {
-        logltffnlf(WARNING, "%s\n", "Filename is not set!");
-        logltffnlf(NOTICE,
-                   "%s\n",
-                   "Filename not being set may result in log files "
-                   "conflicting with one another as there is no name to "
-                   "differentiate what log file is for what.  Because of so, "
-                   "it may be hard to maintain and read the log files as you "
-                   "may not know what the log file is for.");
-    }
+    strftime(tmpfno, NAME_MAX, fnsfxfmt, ti);
 
-    logltffnlf(INTERN_DEBUG, "%s\n", "time()");
-    time_t t        = time(0);
+    size_t tmpfnosz = sprintf(tmpfno, "%s-%s", tmpfno, fname);
 
-    logltffnlf(INTERN_DEBUG, "%s\n", "localtime()");
-    struct tm *ti   = localtime(&t);
-    if (!ti) { fatalstr(strerror(errno)); }
+    realloc(tmpfno, tmpfnosz + 1lu);
+    mallstr(tmpfno, &fnout);
 
-    logltffnlf(INTERN_DEBUG, "malloc(%lu)\n", NAME_MAX);
-    char *tmpfno    = malloc(NAME_MAX);
-    if (!tmpfno) { fatalf("Failed to allocate memory for: %s!\n", "tmpfno"); }
-
-    logltffnlf(INTERN_DEBUG, "%s\n", "strftime()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG,
-               "              char    *s      = tmpfno(%s)\n",
-               tmpfno);
-    logltffnlf(DEV_INTERN_DEBUG,
-               "              size_t  max     = NAME_MAX(%lu)\n",
-               NAME_MAX);
-    logltffnlf(DEV_INTERN_DEBUG,
-               "const         char    *format = fnsfxfmt(%s)\n",
-               fnsfxfmt);
-    logltffnlf(DEV_INTERN_DEBUG,
-               "const struct  tm      *tm     = %s\n", "ti");
-#endif  /* LGR_DEV                */
-    size_t tmpfnosz = strftime(tmpfno, NAME_MAX, fnsfxfmt, ti);
-    if (!tmpfnosz) { fatalf("%s can not be 0!\n", "tmpfno"); }
-
-    CALLFN_MSGLS(INTERN_TRACE, "sprintf()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "      char *str     = tmpfno(%s)\n", tmpfno);
-    logltffnlf(DEV_INTERN_DEBUG, "const char *format  = %s\n", "\"%s-%s\"");
-    logltffnlf(DEV_INTERN_DEBUG, "      ...           = tmpfno(%s)\n", tmpfno);
-    logltffnlf(DEV_INTERN_DEBUG, "      ...           = fname(%s)\n", fname);
-#endif  /* LGR_DEV                */
-    tmpfnosz        = sprintf(tmpfno, "%s-%s", tmpfno, fname);
-    if (!tmpfnosz) { fatalf("%s can not be 0!\n", "tmpfno"); }
-
-    logltffnlf(INTERN_DEBUG, "%s\n", "realloc()");
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG,
-               "void    *ptr  = tmpfno(%lu)\n",
-               strlen(tmpfno));
-    logltffnlf(DEV_INTERN_DEBUG,
-               "size_t  size  = tmpfnosz(%lu) + 1\n",
-               tmpfnosz);
-#endif  /* LGR_DEV                */
-    if (!realloc(tmpfno, tmpfnosz + 1)) {
-        fatalf("Failed to reallocate: %s(%lu) to: %lu(%s) + 1!\n",
-               "tmpfno",
-               strlen(tmpfno),
-               tmpfnosz,
-               "tmpfnosz");
-    }
-
-        CALLFN_MSGLS(INTERN_TRACE, "mallstr()");
-#ifdef  LGR_DEV
-        MALLSTR_DEVMSGSS(tmpfno, fnout);
-#endif  /* LGR_DEV                */
-    mallstr(tmpfno, &fnout, "fnout");
-
-    SET_MSGLSS(INTERN_INFO, fnout, tmpfno);
     fnout = tmpfno;
+    fout  = fopen(fnout, "a");
 
-    logltffnlf(INTERN_INFO, "Updating fout(%s)...\n", fnout);
-
-    logltffnlf(INTERN_DEBUG,
-               "Opening file fout(%s) in append mode...\n",
-               fnout);
-    fout = fopen(fnout, "a");
-    if (!fout) {
-        fatalf("Failed to open %s! %s(%d)\n", fnout, strerror(errno), errno);
-    }
-
-    R_MSGLS(INTERN_DEBUG, fnout);
     return fnout;
 }
 
 char*
 setfilename(char *filename)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "char *filename = %s\n", filename);
-#endif  /* LGR_DEV                */
+    if (!filename || !ltf) { return fname; }
 
-    if (!ltf) { logltffnlf(WARNING, "%s\n", "Logging to file is not set..."); }
-    if (!filename)
-    {
-        logltffnlf(WARNING, "%s\n", "No filename specified...");
+    mallstr(filename, &fname);
 
-        R_MSGLS(INTERN_DEBUG, fname);
-        return fname;
-    }
-
-    CALLFN_MSGLS(INTERN_TRACE, "mallstr()");
-#ifdef  LGR_DEV
-    MALLSTR_DEVMSGSS(filename, fname);
-#endif  /* LGR_DEV                */
-    mallstr(filename, &fname, "fname");
-
-    SET_MSGLSS(INTERN_INFO, fname, filename);
     fname = filename;
 
-    CALLFN_MSGLS(INTERN_TRACE, "setfout()");
     setfout();
 
-    R_MSGLS(INTERN_DEBUG, fname);
     return fname;
 }
 
 int
 setlogtofile(int logtofile)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "int logtofile = %d\n", logtofile);
-#endif  /* LGR_DEV                */
-
-    SET_MSGLDD(INTERN_INFO, ltf, logtofile);
-    ltf = logtofile;
-
-    R_MSGLD(INTERN_DEBUG, ltf);
-    return ltf;
+    return (ltf = logtofile);
 }
 
 int
 getlogtofile(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-    GET_MSGLS(INTERN_INFO, "ltf");
-
-    R_MSGLD(INTERN_DEBUG, ltf);
     return ltf;
 }
 
 char*
 setfilenamesuffixfmt(const char *suffixfmt)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG, "const char *suffixfmt = %s\n", suffixfmt);
-#endif  /* LGR_DEV                */
+    time_t    t       = time(0);
+    struct tm *ti     = localtime(&t);
+    char      *tmpfno = malloc(NAME_MAX);
 
-    logltffnlf(INTERN_DEBUG, "%s\n", "time()");
-    time_t t        = time(0);
-
-    logltffnlf(INTERN_DEBUG, "%s\n", "localtime()");
-    struct tm *ti   = localtime(&t);
-    if (!ti) { fatalstr(strerror(errno)); }
-
-    logltffnlf(INTERN_DEBUG, "malloc(%lu)\n", NAME_MAX);
-    char *tmpfno    = malloc(NAME_MAX);
-    if (!tmpfno) { fatalf("Failed to allocate memory for: %s!\n", "tmpfno"); }
-
-#ifdef  LGR_DEV
-    logltffnlf(DEV_INTERN_DEBUG,
-               "              char    *s      = tmpfno(%s)\n",
-               tmpfno);
-    logltffnlf(DEV_INTERN_DEBUG,
-               "              size_t  max     = NAME_MAX(%lu)\n",
-               NAME_MAX);
-    logltffnlf(DEV_INTERN_DEBUG,
-               "const         char    *format = fnsfxfmt(%s)\n",
-               fnsfxfmt);
-    logltffnlf(DEV_INTERN_DEBUG,
-               "const struct  tm      *tm     = %s\n", "ti");
-#endif  /* LGR_DEV                */
     size_t tmpfnosz = strftime(tmpfno, NAME_MAX, fnsfxfmt, ti);
-    if (!tmpfnosz)
-    {
-        logltffnlf(ERROR,
-                   "%s is not a valid of a valid format!  Using current set "
-                   "fnsfxfmt...\n",
-                   suffixfmt);
+    if (!tmpfnosz) { return fnsfxfmt; }
 
-        R_MSGLS(INTERN_DEBUG, fnsfxfmt);
-        return fnsfxfmt;
-
-    }
-
-    CALLFN_MSGLS(INTERN_TRACE, "mallstr()");
-#ifdef  LGR_DEV
-    MALLSTR_DEVMSGSS(suffixfmt, fnsfxfmt);
-#endif  /* LGR_DEV                */
-    mallstr(suffixfmt, &fnsfxfmt, "fnsfxfmt");
-
-    SET_MSGLSS(INTERN_INFO, fnsfxfmt, suffixfmt);
+    mallstr(suffixfmt, &fnsfxfmt);
     strcpy(fnsfxfmt, suffixfmt);
 
-    R_MSGLS(INTERN_DEBUG, fnsfxfmt);
     return fnsfxfmt;
 }
 
 char*
 getfilenamesuffixfmt(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-    GET_MSGLS(INTERN_INFO, "fnsfxfmt");
-
-    R_MSGLS(INTERN_DEBUG, fnsfxfmt);
     return fnsfxfmt;
 }
 
 char*
 getfilenameout(void)
 {
-    INFUNC_MSGL(INTERN_DEBUG);
-    GET_MSGLS(INTERN_INFO, "fnout");
-
-    R_MSGLS(INTERN_DEBUG, fnout);
     return fnout;
 }
